6ac2491345e5ac4604230d87e84d1ee34187b62a
braulio2315 <br1412922@gmail.com>
2025-06-09 13:41:22 -0600

    fines de semana largos pp

--- dist/services/uploadLongWeekendStatsExcel.js
+++ dist/services/uploadLongWeekendStatsExcel.js
@@ -17,24 +17,25 @@ const LongWeekendStats_model_1 = __importDefault(require("../models/LongWeekendS
 function insertLongWeekendStatsFromExcel(data) {
     return __awaiter(this, void 0, void 0, function* () {
         const formatted = data.map((row) => {
-            var _a, _b, _c;
+            var _a, _b;
             return ({
                 year: parseInt(row['Año']),
-                bridge_name: (_a = row['Puente']) === null || _a === void 0 ? void 0 : _a.toString().trim(),
+                bridge_name: (_a = row['Fin de semana largo']) === null || _a === void 0 ? void 0 : _a.toString().trim(),
                 municipality: (_b = row['Municipio']) === null || _b === void 0 ? void 0 : _b.toString().trim(),
-                occupancy_rate: parseFloat(row['% Ocupación']),
-                room_offer: parseInt(row['Oferta Cuartos']),
-                occupied_rooms: parseInt(row['Cuartos Ocupados']),
-                available_rooms: parseInt(row['Cuartos Disponibles']),
-                average_stay: parseFloat(row['Estadía Promedio']),
-                occupancy_density: parseFloat(row['Densidad de Ocupación']),
+                occupancy_rate: parseFloat(row['Tasa de ocupación']),
+                room_offer: parseInt(row['Oferta cuartos']),
+                occupied_rooms: parseFloat(row['Cuartos ocupados']),
+                available_rooms: parseInt(row['Cuartos disponibles']),
+                average_stay: parseFloat(row['Estadía promedio']),
+                occupancy_density: parseFloat(row['Densidad de ocupación']),
                 nights: parseInt(row['Noches']),
-                tourists_per_night: parseInt(row['Turistas por Noche']),
-                daily_avg_spending: parseFloat(row['Gasto Diario Promedio']),
-                economic_impact: parseInt((_c = row['Derrama Económica']) === null || _c === void 0 ? void 0 : _c.toString().replace(/,/g, '')),
-                tourist_flow: parseInt(row['Afluencia Turística']),
+                tourists_per_night: parseFloat(row['Turistas noche']),
+                daily_avg_spending: parseFloat(row['Gasto promedio diario']),
+                economic_impact: parseFloat(row['Derrama económica']),
+                tourist_flow: parseFloat(row['Afluencia turística']),
             });
         }).filter((row) => row.bridge_name);
+        console.log('Filas a insertar:', formatted.length);
         yield LongWeekendStats_model_1.default.bulkCreate(formatted);
     });
 }
--- src/services/uploadLongWeekendStatsExcel.ts
+++ src/services/uploadLongWeekendStatsExcel.ts
@@ -3,22 +3,21 @@ import LongWeekendStats from '../models/LongWeekendStats.model';
 export async function insertLongWeekendStatsFromExcel(data: any[]) {
   const formatted = data.map((row) => ({
     year: parseInt(row['Año']),
-    bridge_name: row['Puente']?.toString().trim(),
+    bridge_name: row['Fin de semana largo']?.toString().trim(),
     municipality: row['Municipio']?.toString().trim(),
-    occupancy_rate: parseFloat(row['% Ocupación']),
-    room_offer: parseInt(row['Oferta Cuartos']),
-    occupied_rooms: parseInt(row['Cuartos Ocupados']),
-    available_rooms: parseInt(row['Cuartos Disponibles']),
-    average_stay: parseFloat(row['Estadía Promedio']),
-    occupancy_density: parseFloat(row['Densidad de Ocupación']),
+    occupancy_rate: parseFloat(row['Tasa de ocupación']),
+    room_offer: parseInt(row['Oferta cuartos']),
+    occupied_rooms: parseFloat(row['Cuartos ocupados']),
+    available_rooms: parseInt(row['Cuartos disponibles']),
+    average_stay: parseFloat(row['Estadía promedio']),
+    occupancy_density: parseFloat(row['Densidad de ocupación']),
     nights: parseInt(row['Noches']),
-    tourists_per_night: parseInt(row['Turistas por Noche']),
-    daily_avg_spending: parseFloat(row['Gasto Diario Promedio']),
-    economic_impact: parseInt(
-      row['Derrama Económica']?.toString().replace(/,/g, '')
-    ),
-    tourist_flow: parseInt(row['Afluencia Turística']),
+    tourists_per_night: parseFloat(row['Turistas noche']),
+    daily_avg_spending: parseFloat(row['Gasto promedio diario']),
+    economic_impact: parseFloat(row['Derrama económica']),
+    tourist_flow: parseFloat(row['Afluencia turística']),
   })).filter((row) => row.bridge_name);
 
+  console.log('Filas a insertar:', formatted.length);
   await LongWeekendStats.bulkCreate(formatted);
 }
