2a86f02609a1c3babd2cda4e152dca66fc71679c
braulio2315 <br1412922@gmail.com>
2025-08-08 13:15:22 -0600

    tablas de month

--- src/routes/monthlyStats.ts
+++ src/routes/monthlyStats.ts
@@ -23,8 +23,11 @@ router.post(
       res.status(200).json({ message: `Archivo  procesado correctamente.` });
     } catch (error) {
       console.error(error);
-      console.log(error);
-      res.status(500).json({ message: 'Error procesando el archivo.' });
+      res.status(500).json({
+        message: 'Error procesando el archivo.',
+        error: error instanceof Error ? error.message : String(error),
+        stack: error instanceof Error ? error.stack : undefined
+      });
     }
   }
 );
