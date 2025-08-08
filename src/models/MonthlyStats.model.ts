4587eaa411ee0df3fcb1b14c83dfd7f0bf8495ed
braulio2315 <br1412922@gmail.com>
2025-08-08 13:12:24 -0600

    tablas de month

--- src/models/MonthlyStats.model.ts
+++ src/models/MonthlyStats.model.ts
@@ -1,11 +1,28 @@
 import { Table, Column, Model, DataType } from 'sequelize-typescript';
 
-@Table({ tableName: 'monthly_stats' })
+@Table({ tableName: 'monthly_stats', timestamps: false })
 export default class MonthlyStats extends Model {
-  @Column({ type: DataType.INTEGER, allowNull: false }) year!: number;
-  @Column({ type: DataType.STRING, allowNull: false }) month!: string;
-  @Column({ type: DataType.STRING, allowNull: false }) municipality!: string;
-  @Column({ type: DataType.FLOAT, allowNull: false }) occupancyRate!: number;
-  @Column({ type: DataType.INTEGER, allowNull: false }) touristFlow!: number;
-  @Column({ type: DataType.BIGINT, allowNull: false }) economicImpact!: number;
+  @Column({ type: DataType.INTEGER, allowNull: false })
+  year!: number;
+
+  @Column({ type: DataType.STRING, allowNull: false })
+  month!: string;
+
+  @Column({ type: DataType.STRING, allowNull: false })
+  municipality!: string;
+
+  @Column({ type: DataType.FLOAT, allowNull: false, field: 'occupancyRate' })
+  occupancyRate!: number;
+
+  @Column({ type: DataType.INTEGER, allowNull: false, field: 'touristFlow' })
+  touristFlow!: number;
+
+  @Column({ type: DataType.BIGINT, allowNull: false, field: 'economicImpact' })
+  economicImpact!: number;
+
+  @Column({ type: DataType.DATE, allowNull: false, field: 'createdAt' })
+  createdAt!: Date;
+
+  @Column({ type: DataType.DATE, allowNull: false, field: 'updatedAt' })
+  updatedAt!: Date;
 }
