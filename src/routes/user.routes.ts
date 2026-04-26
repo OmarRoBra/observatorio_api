f4c02af8ef9ae1d77c2394f0af9f8472ef223700
braulio rodriguez <br1412922@gmail.com>
2026-04-26 01:43:44 -0600

    arreglo de rutas de routes

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,11 +1,20 @@
 import express from 'express';
-import { getUser, updateUser,deleteUser } from '../controllers/user.controller';
+import { getAllUsers, getUser, createUser, updateUser, deleteUser } from '../controllers/user.controller';
 import { authMiddleware } from '../middleware/auth.middleware';
 
 const router = express.Router();
 
+// 1. PRIMERO LAS RUTAS ESTÁTICAS (Para evitar que /:id se trague a /me)
 router.get('/me', authMiddleware, getUser);
-// Descomentar la ruta PUT y añadir DELETE
 router.put('/me', authMiddleware, updateUser);
 router.delete('/me', authMiddleware, deleteUser);
+
+// 2. RUTAS GENERALES
+router.get('/', authMiddleware, getAllUsers);
+router.post('/', authMiddleware, createUser);
+
+// 3. AL FINAL LAS RUTAS DINÁMICAS CON PARÁMETROS (/:id)
+router.put('/:id', authMiddleware, updateUser);
+router.delete('/:id', authMiddleware, deleteUser);
+
 export default router;
