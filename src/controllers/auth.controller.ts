229299105841f4c92d1b33fe9d80ff63bc458da4
braulio rodriguez <br1412922@gmail.com>
2026-04-30 19:13:02 -0600

    Agrega soporte para seleccionar el rol (admin/editor) al registrar usuarios

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -4,11 +4,12 @@ import  User  from '../models/user.model';
 import { generateToken } from '../utils/jwt';
 
 export const register = async (req: Request, res: Response): Promise<void> => {
-  const { name, email, password } = req.body;
+  const { name, email, password, role } = req.body;
 
   try {
     const hashedPassword = await bcrypt.hash(password, 10);
-    const user = await User.create({ name, email, password: hashedPassword, role: "admin" });
+    const userRole = role === "admin" ? "admin" : "editor"; // por defecto editor si no es admin
+    const user = await User.create({ name, email, password: hashedPassword, role: userRole });
     res.status(201).json({ message: 'User registered', user });
   } catch (error) {
     res.status(500).json({ message: 'Error registering user', error });
